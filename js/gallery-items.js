612b83f16d3245455f962d8da9a1055ed7c4d014
Khrystyna-Hats <christina.hats18@gmail.com>
2022-01-30 23:04:17 +0200

    upd

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -64,42 +64,6 @@ export const galleryItems = [
   },
 ];
 
-const galleryContainer = document.querySelector('.gallery');
-const itemsMarkup = createGalleryMarkup(galleryItems);
-
-galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
-
-function createGalleryMarkup(galleryItems) { 
-  return galleryItems.map(({preview, original, description}) => { 
-        return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>`
-  }).join('');
-}
-
-
-  galleryContainer.addEventListener('click',onImgClick);
-
-function onImgClick(event) { 
-  preventDefault();
-
-  if (!event.target.contains('g_allery_item')) {
-    return;
-  };
-
-  basicLightbox.create(`
-		<img width="1400" height="900" src="${galleryItems.preview}">
-	`).show()
-
-  // console.log(galleryItems.original)
-  };
 
 
 
